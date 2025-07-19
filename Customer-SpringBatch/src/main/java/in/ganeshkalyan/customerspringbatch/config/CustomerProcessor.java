16690c90e5c0ea4d0ef168231327aa2b689d39fd
ganeshkalyank <srikgk333@gmail.com>
2025-07-19 23:11:05 +0530

    added batch app

--- Customer-SpringBatch/src/main/java/in/ganeshkalyan/customerspringbatch/config/CustomerProcessor.java
+++ Customer-SpringBatch/src/main/java/in/ganeshkalyan/customerspringbatch/config/CustomerProcessor.java
@@ -1,4 +1,16 @@
 package in.ganeshkalyan.customerspringbatch.config;
 
-public class CustomerProcessor {
+import in.ganeshkalyan.customerspringbatch.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
+
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        if(customer.getCountry().equals("United States")) {
+            return customer;
+        }else{
+            return null;
+        }
+    }
 }
--- Customer-SpringBatch/src/main/java/in/ganeshkalyan/customerspringbatch/repository/CustomerRepository.java
+++ Customer-SpringBatch/src/main/java/in/ganeshkalyan/customerspringbatch/repository/CustomerRepository.java
@@ -1,4 +1,7 @@
 package in.ganeshkalyan.customerspringbatch.repository;
 
-public class CustomerRepository {
+import in.ganeshkalyan.customerspringbatch.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
 }
